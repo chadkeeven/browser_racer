32f170876ed7f381cd7e8cfca347213d72c44f35
chadkeeven <chadkeeven@gmail.com>
2017-12-10 12:06:12 -0700

    p1 box moves on keydown

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-		
+	console.log("document is ready");
+	//var pOneMargin = $("#playerOne").css("margin-left");
+	var pOneMargin = 0;
+	console.log(pOneMargin); 
+	$(this).keydown(function() {
+		pOneMargin += 5;
+		$("#playerOne").css('margin-left', pOneMargin + '%');
+		console.log(pOneMargin);
+		console.log("playerOne advance");
+	});
 });
