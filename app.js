f588094ec9f001dd80f29034b87fe6d9831be9df
chadkeeven <chadkeeven@gmail.com>
2017-12-10 16:35:47 -0700

    added background color and insturctions

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+//make sure document is ready before adding JS
 $(document).ready(function() {
-	//console.log("document is ready");
-	//var pOneMargin = $("#playerOne").css("margin-left");
+	//margin for playerOne, used to move block
 	var pOneMargin = 0;
-	//console.log(pOneMargin); 
+	//event listener for key press to move box
 	$(this).keyup(function(){
-		addMarginToOne();		
+		addMarginToOne(); 		
 	});
+	/*function that adds 5% margin to
+	playone box, checks if less than 100% margin.
+	once margin is equal to or greater than 100% 
+	an alert is sent to tell player one won.
+	If 100% margin has been met then an alert 
+	gives the users feedback telling them the game has ended.
+	*/ 
 	function addMarginToOne(){
 
-		if (pOneMargin < 90){
+		if (pOneMargin < 100){
 			pOneMargin += 5;
 			//console.log(pOneMargin);
 			$("#playerOne").css('margin-left', pOneMargin + '%');
-			if (pOneMargin >= 90){
+			if (pOneMargin >= 100){
 				setTimeout(function(){alert("Player One Won!");}, 500);
 			}
 		}else{
