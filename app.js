2b18768ad6d1ed4cecfc6a03f6631a468e175c61
chadkeeven <chadkeeven@gmail.com>
2017-12-10 15:28:26 -0700

    winning condition added

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
-	console.log("document is ready");
+	//console.log("document is ready");
 	//var pOneMargin = $("#playerOne").css("margin-left");
 	var pOneMargin = 0;
-	console.log(pOneMargin); 
-	$(this).keydown(function() {
-		pOneMargin += 5;
-		$("#playerOne").css('margin-left', pOneMargin + '%');
-		console.log(pOneMargin);
-		console.log("playerOne advance");
+	//console.log(pOneMargin); 
+	$(this).keyup(function(){
+		addMarginToOne();		
 	});
+	function addMarginToOne(){
+
+		if (pOneMargin < 90){
+			pOneMargin += 5;
+			console.log(pOneMargin);
+			$("#playerOne").css('margin-left', pOneMargin + '%');
+			if (pOneMargin >= 90){
+				setTimeout(function(){alert("Player One Won!");}, 500);
+			}
+		}
+	}
 });
