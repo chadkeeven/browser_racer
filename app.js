8c79545746eac65a045cec6243f194d4e7ea9c3f
chadkeeven <chadkeeven@gmail.com>
2017-12-11 14:37:00 -0700

    made the l key for playerer 2

--- app.js
+++ app.js
@@ -2,13 +2,18 @@
 $(document).ready(function() {
 	//margin for playerOne, used to move block
 	var pOneMargin = 0;
+	//margin for playerTwo, used to move block
+	var pTwoMargin = 0;
 	//event listener for key press to move box
 	$(this).keyup(function(event){
 		//tests if key up was 'a'
 		if (event.which === 65) {
-			addMarginToOne();
-		} 		
-	});
+			addMargin(pOneMargin, 1);
+		//tests if key up was 'l'
+	}else if (event.which ===76) {
+		addMargin(pTwoMargin, 2);
+	}		
+});
 	/*function that adds 5% margin to
 	playone box, checks if less than 100% margin.
 	once margin is equal to or greater than 100% 
@@ -16,15 +21,23 @@ $(document).ready(function() {
 	If 100% margin has been met then an alert 
 	gives the users feedback telling them the game has ended.
 	*/ 
-	function addMarginToOne(){
-
-		if (pOneMargin < 100){
-			pOneMargin += 5;
-			//console.log(pOneMargin);
-			$("#playerOne").css('margin-left', pOneMargin + '%');
-			if (pOneMargin >= 100){
-				setTimeout(function(){alert("Player One Won!");}, 500);
+	function addMargin(playerMargin, playerNumber){
+		if (playerNumber === 1) {
+			if (pOneMargin < 100){
+				pOneMargin += 5;
+				$("#playerOne").css('margin-left', pOneMargin + '%');
+				if (pOneMargin >= 100){
+					setTimeout(function(){alert("Player One Won!");}, 500);
+				}
 			}
+		}else if (playerNumber === 2) {
+			if (pTwoMargin < 100){
+				pTwoMargin += 5;
+				$("#playerTwo").css('margin-left', pTwoMargin + '%');
+				if (pTwoMargin >=  100){
+					setTimeout(function(){alert("Player Two Won!");}, 500);
+				}
+			}	
 		}else{
 			alert("Game has ended");
 		}
