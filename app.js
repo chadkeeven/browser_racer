74276bc5c4e6342be64420b9826383524b5b05ec
chadkeeven <chadkeeven@gmail.com>
2017-12-11 14:54:06 -0700

    checkForWinner function added

--- app.js
+++ app.js
@@ -8,38 +8,48 @@ $(document).ready(function() {
 	$(this).keyup(function(event){
 		//tests if key up was 'a'
 		if (event.which === 65) {
-			addMargin(pOneMargin, 1);
+			addMargin(1);
 		//tests if key up was 'l'
 	}else if (event.which ===76) {
-		addMargin(pTwoMargin, 2);
+		addMargin(2);
 	}		
 });
 	/*function that adds 5% margin to
-	playone box, checks if less than 100% margin.
+	the playerNumber's box, checks if less than 100% margin.
 	once margin is equal to or greater than 100% 
 	an alert is sent to tell player one won.
 	If 100% margin has been met then an alert 
 	gives the users feedback telling them the game has ended.
 	*/ 
-	function addMargin(playerMargin, playerNumber){
-		if (playerNumber === 1) {
-			if (pOneMargin < 100){
-				pOneMargin += 5;
-				$("#playerOne").css('margin-left', pOneMargin + '%');
-				if (pOneMargin >= 100){
-					setTimeout(function(){alert("Player One Won!");}, 500);
+	function addMargin(playerNumber){
+		if(!checkForWinner()){
+			if (playerNumber === 1) {
+				if (pOneMargin < 100){
+					pOneMargin += 5;
+					$("#playerOne").css('margin-left', pOneMargin + '%');
+					if (pOneMargin >= 100){
+						setTimeout(function(){alert("Player One Won!");}, 500);
+					}
 				}
+			}else if (playerNumber === 2) {
+				if (pTwoMargin < 100){
+					pTwoMargin += 5;
+					$("#playerTwo").css('margin-left', pTwoMargin + '%');
+					if (pTwoMargin >=  100){
+						setTimeout(function(){alert("Player Two Won!");}, 500);
+					}
+				}	
 			}
-		}else if (playerNumber === 2) {
-			if (pTwoMargin < 100){
-				pTwoMargin += 5;
-				$("#playerTwo").css('margin-left', pTwoMargin + '%');
-				if (pTwoMargin >=  100){
-					setTimeout(function(){alert("Player Two Won!");}, 500);
-				}
-			}	
 		}else{
 			alert("Game has ended");
+		}
+	}
+	//functionto check if there has been a winner to the game!
+	function checkForWinner(){
+		if(pOneMargin === 100 || pTwoMargin === 100){
+			return true;
+		}else{
+			return false;
 		}
 	}
 });
